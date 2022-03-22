a613d51d0434399b68bb30981c49cdd5d5e7a474
Jamil Moughal <jamil.moughal@usscyber.com>
2022-03-23 01:19:02 +0500

    compilation errors fixed

--- Ballot2/contracts/Ballot.sol
+++ Ballot2/contracts/Ballot.sol
@@ -1,18 +1,17 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.5.16;
 
 contract Ballot {
-
     struct Voter {
-        uint weight;
+        uint256 weight;
         bool voted;
         uint8 vote;
         // address delegate;
     }
 
     //modifer
-    modifier onlyOwner () {
-      require(msg.sender == chairperson);
-      _;
+    modifier onlyOwner() {
+        require(msg.sender == chairperson);
+        _;
     }
 
     /* struct Proposal {
@@ -20,18 +19,23 @@ contract Ballot {
     } */
     address public chairperson;
     mapping(address => Voter) public voters;
-    uint[4] public proposals;
+    uint256[4] public proposals;
 
     // Create a new ballot with 4 different proposals.
-    function Ballot() public {
+    // function Ballot() public {
+    //     chairperson = msg.sender;
+    //     voters[chairperson].weight = 2;
+    // }
+
+    constructor() public {
         chairperson = msg.sender;
         voters[chairperson].weight = 2;
     }
 
     /// Give $(toVoter) the right to vote on this ballot.
     /// May only be called by $(chairperson).
-    function register(address toVoter) public onlyOwner{
-        if(voters[toVoter].weight != 0) revert();
+    function register(address toVoter) public onlyOwner {
+        if (voters[toVoter].weight != 0) revert();
         voters[toVoter].weight = 1;
         voters[toVoter].voted = false;
     }
@@ -45,7 +49,7 @@ contract Ballot {
         proposals[toProposal] += sender.weight;
     }
 
-    function winningProposal() public constant returns (uint8 _winningProposal) {
+    function winningProposal() public view returns (uint8 _winningProposal) {
         uint256 winningVoteCount = 0;
         for (uint8 prop = 0; prop < 4; prop++)
             if (proposals[prop] > winningVoteCount) {
@@ -54,7 +58,10 @@ contract Ballot {
             }
     }
 
-    function getCount() public constant returns (uint[4]) {
-        return proposals;
+    // function getCount() public view returns (uint256[4]) {
+    //     return proposals;
+    // }
+    function getCount(uint256 index) public view returns (uint256) {
+        return proposals[index];
     }
 }
